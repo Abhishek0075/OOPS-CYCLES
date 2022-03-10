572169024877eed0aeda4be5921f69729a416572
Abhishek <95518276+Abhishek0075@users.noreply.github.com>
2022-03-10 10:30:42 +0530

    corrected 3

---
+++ Cycle 1/3.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+class set{
+	int rollNo,Mark1,Mark2,Mark3;
+	float avg;
+	string name;
+public:
+	void input(void);
+	void display(void);
+	void calcGrade(void);
+	void average(){
+		avg=(Mark1 + Mark2+Mark3)/3;
+	}
+};
+void set::calcGrade(void){
+	if(avg>=90){
+		cout<<"A+";
+	}else if(avg<90 && avg>=80){
+		cout<<"A";
+	}else if(avg<80 && avg>=70){
+		cout<<"B+";
+	}else if(avg<70 && avg>=60){
+		cout<<"B";
+	}else if(avg<60 && avg>=50){
+		cout<<"C+";
+	}else if(avg<50 && avg>=40){
+		cout<<"C";
+	}else if(avg<40 && avg>=30){
+		cout<<"D+";
+	}else{
+		cout<<"Failed";
+	}
+}
+void set::input(void){
+	cout<<"========INPUT DETAILS========"<<endl;
+	cout<<"Input Name "<<endl;
+	cin>>name;
+	cout<<"Input Roll No"<<endl;
+	cin>>rollNo;
+	cout<<"Input Marks of 3 subjects"<<endl;
+	cin>>Mark1;
+	while(Mark1>100){
+		cout<<"!! Mark is greater than 100 Enter right marks !!"<<endl;
+		cin>>Mark1;
+		if(Mark1<=100){
+			break;
+		}
+	}
+	cin>>Mark2;
+	while(Mark2>100){
+		cout<<"!! Mark is greater than 100 Enter right marks !!"<<endl;
+		cin>>Mark2;
+		if(Mark2<=100){
+			break;
+		}
+	}
+	cin>>Mark3;
+	while(Mark3>100){
+		cout<<"!! Mark is greater than 100 Enter right marks !!"<<endl;
+		cin>>Mark3;
+		if(Mark3<=100){
+			break;
+		}
+	}
+}
+void set::display(void){
+	cout<<"-------REPORT-------"<<endl;
+	cout<<"  Name : "<<name<<endl;
+	cout<<"  Roll No : "<<rollNo<<endl;
+	cout<<"  Average marks : "<<avg<<endl;
+	cout<<"  Grade : ";
+}
+int main() {
+	int x,i,j,e;
+	cout<<"Enter the number of students"<<endl;
+	cin>>x;
+	set A[x];
+	for(i=0;i<x;i++){
+		A[i].input();
+		cout<<"Enter 0 to exit or any other number to continue input of details : ";
+		cin>>e;
+		if(e==0){
+			break;
+		}
+	}
+	for(j=0;j<=i;j++){
+		A[j].average();
+		A[j].display();
+		A[j].calcGrade();
+		cout<<endl;
+	}
+	return 0;
+}
