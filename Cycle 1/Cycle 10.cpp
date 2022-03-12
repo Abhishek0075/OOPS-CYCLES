889734281422ea1a56075af3203e7cc2b071168d
Abhishek P <abhirajp075@gmail.com>
2022-03-12 19:03:32 +0530

    updated

--- Cycle 1/Cycle 10.cpp
+++ Cycle 1/Cycle 10.cpp
@@ -1,74 +1,90 @@
-//============================================================================
-// Name        : Cycle.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-//============================================================================
-// Name        : test3.cpp
-// Author      :
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 using namespace std;
-const int m=100;
+int count=0;
 class items{
-	int itemcode[m];
-	float itemprice[m];
-	int count;
+	int itemcode;
+	float itemprice;
 public:
-	void CNT(){
-		count=0;
-	}
-	void additem(){
+	void additem();
+	void remove();
+	void sumup();
+	void displayitems();
+};
+items prdt[100];
+void items:: additem(){
+	cout<<"============ADD ITEM==========="<<endl;
 		cout<<"Enter item code : ";
-		cin>>itemcode[count];
+		cin>>itemcode;
 		cout<<"Enter the item price : ";
-		cin>>itemprice[count];
+		cin>>itemprice;
 		count++;
 	}
-	void sumup(){
-		float sum=0;
-		for(int i=0;i<count;i++){
-			sum=sum+itemprice[i];
-		}
-		cout<<"============================"<<endl<<"TOTAL PRICE = "<<sum<<endl;
+
+void items::sumup(){
+	float sum=0;
+	for(int i=0;i<count;i++){
+		sum=sum+prdt[i].itemprice;
 	}
-	void displayitems(){
-		int j=0;
-		for(int i=0;i<count;i++){
-			if(itemprice[i]>0){
-				j=j+1;
-				cout<<"=======ITEM "<<j<<"======="<<endl;
-				cout<<"Code : "<<itemcode[i]<<endl;
-				cout<<"Price : "<<itemprice[i]<<endl;
-			}
-		}
-	}
-	void remove(){
-		int x;
-		cout<<"Enter the code to be deleted :";
-		cin>>x;
-		for(int i=0;i<count;i++){
-			if(itemcode[i]==x){
-				itemprice[i]=0;
-			}
+	cout<<"============================"<<endl<<"TOTAL PRICE = "<<sum<<endl;
+}
+void items::remove(){
+	cout<<"============REMOVE ITEM==========="<<endl;
+	int x;
+	cout<<"Enter the code to be deleted :";
+	cin>>x;
+	for(int i=0;i<count;i++){
+		if(prdt[i].itemcode==x){
+			prdt[i].itemprice=0;
 		}
 	}
-};
+}
+void items::displayitems(){
+	int j=0;
+	for(int i=0;i<count;i++){
+		if(prdt[i].itemprice>0){
+			j=j+1;
+			cout<<"=======ITEM "<<j<<"======="<<endl;
+			cout<<"Code : "<<prdt[i].itemcode<<endl;
+			cout<<"Price : "<<prdt[i].itemprice<<endl;
+		}
+	}
+}
 int main() {
-	items order;
-	order.CNT();
-	order.additem();
-	order.additem();
-	order.additem();
-	order.remove();
-	order.displayitems();
-	order.sumup();
+	int x=3,y;
+	while(x>=1 and x<=5){
+				cout<<"=======================SHOPPING LIST======================="<<endl;
+				cout<<"\tNUMBER        PROCESS"<<endl;
+				cout<<"\t1 	 : Add an item ";
+				cout<<"\n\t2 	 : Delete an item";
+				cout<<"\n\t3 	 : Display all items";
+				cout<<"\n\t4 	 : Show the items with the total sum of prices";
+				cout<<"\n\tANY 	 : Quit";
+				cout<<"\n\tENTER YOUR INPUT >>> ";
+				cin>>x;
+		if(x==1){
+			prdt[count].additem();
+		}else if(x==2){
+			prdt[count].remove();
+		}else if(x==3){
+			prdt[count].displayitems();
+		}else if(x==4){
+			prdt[count].displayitems();
+			prdt[count].sumup();
+		}else{
+			cout<<"Thank You"<<endl;
+			break;
+		}
+		cout<<"========================================="<<endl;
+		cout<<"       DO YOU WANT TO CONTINUE"<<endl<<"Press 1 to continue or any other to exit  "<<endl<<">>>>";
+		cin>>y;
+		if(y==1){
+
+		}else{
+			cout<<"Thank You"<<endl;
+			break;
+		}
+	}
 	return 0;
 }
 
