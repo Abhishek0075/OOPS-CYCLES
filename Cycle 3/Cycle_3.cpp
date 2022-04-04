09b28a9beaca0dcb8e0d69b7b082a9f69bb6c9c5
Abhishek P <abhirajp075@gmail.com>
2022-04-04 22:55:41 +0530

    cycle_3_3 added

---
+++ Cycle 3/Cycle_3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float num1,num2;
+    cout<<".............FIND a/(a-b)............."<<endl;
+    cout<<"Enter a and b numbers :- "<<endl;
+    cout<<">>> ";
+    cin>>num1;
+    cout<<">>> ";
+    cin>>num2;
+    float x=num1-num2;
+    try{
+        if(x!=0){
+            cout<<"Result of a/(a-b): "<<(num1/x)<<endl;
+        }else{
+            throw(x);
+        }
+    }catch(float i){
+        cout<<"Exception caught : denominator = "<<x<<endl;
+    }
+    cout<<".......END.......";
+
+}
