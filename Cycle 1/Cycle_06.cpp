031d1798fb3a460ff3bf56affe21db95eb0999ff
Abhishek <95518276+Abhishek0075@users.noreply.github.com>
2022-04-05 14:57:41 +0530

    Update Cycle_06.cpp

--- Cycle 1/Cycle_06.cpp
+++ Cycle 1/Cycle_06.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-class time{
+class Time{
 	int seconds,minutes,hours;
 public:
 	void gettime(){
@@ -9,7 +9,7 @@ public:
 		cin>>minutes;
 		cin>>seconds;
 	}
-	void sum(time t1,time t2){
+	void sum(Time t1,Time t2){
 		seconds=t1.seconds+t2.seconds;
 		minutes=seconds/60;
 		seconds=seconds%60;
@@ -23,7 +23,7 @@ public:
 	}
 };
 int main() {
-	time T1,T2,T3;
+	Time T1,T2,T3;
 	T1.gettime();
 	T2.gettime();
 	T3.sum(T1,T2);
